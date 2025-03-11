e3790823f2ac38dfa0759378412e9543dd938b09
Othmane <othmane.menkor@student-cs.fr>
2025-03-11 12:45:52 +0100

    Countdown event

---
+++ app/events/countdown.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import Countdown from "react-countdown";
+
+const Countdowncomp = () => {
+  return <Countdown date={"2025-03-12T19:45:00"} />;
+};
+
+export default Countdowncomp;
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,11 +1,13 @@
 import { Fade } from "react-awesome-reveal";
+import Countdowncomp from "./countdown";
 
 export default async function EventsPage() {
   return (
     <>
-      <div className="h-screen text-3xl flex items-center justify-center text-center">
-        <Fade>
-          <p> UNDER CONSTRUCTION </p>
+      <div className="h-screen text-[#380f43] text-7xl flex flex-col items-center justify-center text-center gap-y-9">
+        <Fade cascade triggerOnce>
+          <p> Next Event In </p>
+          <Countdowncomp />
         </Fade>
       </div>
     </>
