f95ab818cde4e42d9fef9f290764126ff776dc30
Othmane <othmane.menkor@student-cs.fr>
2025-03-11 09:54:15 +0100

    Added Carousel for companies

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Companies from "./Components/companies";
 import Herotwo from "./Components/herotwo";
 import Whatwedo from "./Components/whatwedo";
+import InfiniteLogoSlider from "./Components/widgets/InfiniteLogoSlider";
 
 export default async function Page() {
   return (
     <>
       <div className="pt-[80px] pb-[60px]">
         <Herotwo />
-
-        <Companies />
+        <InfiniteLogoSlider />
         <Whatwedo />
       </div>
     </>
