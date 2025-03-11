03088dc68f1061100ea73350a4ec1e1988700721
Othmane <othmane.menkor@student-cs.fr>
2025-03-11 10:04:23 +0100

    logo slider for mobile

--- app/Components/widgets/InfiniteLogoSlider.tsx
+++ app/Components/widgets/InfiniteLogoSlider.tsx
@@ -6,7 +6,7 @@ import Slider from "react-infinite-logo-slider";
 const InfiniteLogoSlider = () => {
   return (
     <>
-      <Fade triggerOnce cascade>
+      <Fade triggerOnce cascade className="hidden md:block">
         <div className="w-[80%] mx-auto text-center mb-15">
           <p className="text-2xl ">Trusted by leading Tech companies </p>
         </div>
@@ -14,6 +14,72 @@ const InfiniteLogoSlider = () => {
         <Slider
           width="250px"
           duration={28}
+          pauseOnHover={false}
+          blurBorders={true}
+          blurBorderColor={"#fcf2ff"}
+        >
+          <Slider.Slide>
+            <Image
+              src="/logos/openai.png"
+              alt="openai"
+              width={140}
+              height={140}
+            />
+          </Slider.Slide>
+          <Slider.Slide>
+            <div className="w-[145px] h-[145px] flex items-center ">
+              <Image
+                src="/logos/mistral.png"
+                alt="mistral"
+                width={70}
+                height={70}
+                className="mx-auto my-auto"
+              />
+            </div>
+          </Slider.Slide>
+          <Slider.Slide>
+            <Image
+              src="/logos/perplexity.png"
+              alt="perplexity"
+              width={145}
+              height={145}
+            />
+          </Slider.Slide>
+          <Slider.Slide>
+            <Image
+              src="/logos/snowflake.png"
+              alt="snowflake"
+              width={120}
+              height={120}
+            />
+          </Slider.Slide>
+          <Slider.Slide>
+            <Image
+              src="/logos/mirakl.png"
+              alt="mirakl"
+              width={120}
+              height={120}
+            />
+          </Slider.Slide>
+          <Slider.Slide>
+            <Image
+              src="/logos/kima.png"
+              alt="kimaventures"
+              width={120}
+              height={120}
+            />
+          </Slider.Slide>
+        </Slider>
+      </Fade>
+
+      <Fade triggerOnce cascade className="block md:hidden">
+        <div className="w-[80%] mx-auto text-center mb-15">
+          <p className="text-2xl ">Trusted by leading Tech companies </p>
+        </div>
+
+        <Slider
+          width="250px"
+          duration={12}
           pauseOnHover={false}
           blurBorders={true}
           blurBorderColor={"#fcf2ff"}
