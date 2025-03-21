c5e8527e18724c2a8cb952da60ba97a8d185f95b
Othmane <othmane.menkor@student-cs.fr>
2025-03-21 12:34:40 +0100

    svg to png for icons

--- app/events/scrolldownindicator.tsx
+++ app/events/scrolldownindicator.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const ScrollDownIndicator = () => {
   return (
-    <div className="absolute bottom-8 animate-bounce">
-      <div className="flex flex-col items-center">
-        <p className="text-sm text-[#380f43] mb-2">Scroll Down</p>
+    <div className="absolute bottom-4 md:bottom-8 w-full flex justify-center z-10">
+      <div className="flex flex-col items-center  py-2 px-4 animate-bounce">
+        <p className="text-xs md:text-sm text-[#380f43] mb-1 md:mb-2 font-medium">
+          Scroll Down
+        </p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
