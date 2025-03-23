c3133dae04449d08525a43b9999e72c591a4b20f
Othmane <othmane.menkor@student-cs.fr>
2025-03-23 23:42:24 +0100

    changed footer

--- app/Components/footer/footer.tsx
+++ app/Components/footer/footer.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 
-import LinkOthmane from "./LinkOthmane";
-
-import ArrowIcon from "./arrowicon";
-
 const Footer = () => {
   return (
     <>
       <nav className="  bg-[#cc8ce6] shadow-sm  bottom-0 w-full z-50 backdrop-blur-sm ">
         <div className="w-full bg-[#cc8ce6] h-16 text-center flex justify-center align-middle items-center text-[12px] gap-1">
-          <p>
-            Made by <LinkOthmane />
-          </p>
-          <ArrowIcon />
+          <p>© 2025 Utopia - All rights reserved</p>
         </div>
       </nav>
     </>
